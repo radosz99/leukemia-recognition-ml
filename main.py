b084ce0f6c16ffd91a3f80f4540af8c3a7639418
radosz99 <radoslaw.lis@nokia.com>
2021-03-31 12:56:56 +0200

    Refactorize code

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import os
-from sklearn.feature_selection import SelectKBest, chi2, f_classif
+from sklearn.feature_selection import SelectKBest, f_classif
 
-dataset_classes = {}
-dataset_columns = []
+classes = {}
+symptoms = []
+file_prefix = 'output/bialaczka_'
+classes_size = 0
 data_list = []
+
 
 def get_lines_from_file(filename):
     with open(filename, 'r', encoding='utf-8') as myfile:
@@ -23,40 +26,33 @@ def read_from_file_to_list(filename):
 
 
 def read_parameters_from_file():
-    global dataset_classes, dataset_columns
-    dataset_classes = read_from_file_to_dict('jednostki.txt')
-    dataset_columns = read_from_file_to_list('cechy.txt')
+    global classes, symptoms
+    classes = read_from_file_to_dict('jednostki.txt')
+    symptoms = read_from_file_to_list('cechy.txt')
 
 
-def build_features_ranking(x, y, score_func):
-    features_num = x.shape[1]
-    k_best_selector = SelectKBest(score_func=score_func, k=features_num)
-    k_best_selector.fit(x, y)
-    scores_ranking = [
-        (name, round(score, 2))
-        for name, score in zip(x.columns, k_best_selector.scores_)
-    ]
-    scores_ranking.sort(reverse=True, key=lambda x: x[1])
-    return scores_ranking
-
-
-def parse_csv(i, file):
-    data_set = pd.read_csv(file, sep=' ', header=None)
-    data_set['Class'] = i
-    return data_set
+def parse_csv(i, filename):
+    with open(f"{filename}{i}.txt", 'r') as myfile:
+        data_set = pd.read_csv(myfile, sep=' ', header=None)
+        data_set['Nr klasy'] = i
+        return data_set
 
 
 def evaluate():
-    data_list = [parse_csv(i, file) for i, file in enumerate(glob.glob('output/*.txt'), 1)]
+    data_list = [parse_csv(i, file_prefix) for i in range(1, classes_size + 1)]
     dataset = pd.concat(data_list, axis=0)
-    dataset.columns = dataset_columns
+    dataset.columns = symptoms
     return create_ranking(dataset)
 
 
 def create_ranking(dataset):
-    x = dataset.drop('Class', axis=1)
-    y = dataset['Class']
-    return build_features_ranking(x, y, f_classif)
+    x = dataset.drop('Nr klasy', axis=1)
+    y = dataset['Nr klasy']
+    k_best_selector = SelectKBest(score_func=f_classif, k=len(symptoms) - 1)
+    k_best_selector.fit(x, y)
+    scores_ranking = [(symptom, round(score, 2)) for symptom, score in zip(x.columns, k_best_selector.scores_)]
+    scores_ranking.sort(reverse=True, key=lambda x: x[1])
+    return scores_ranking
 
 
 def divide_data_to_files():
@@ -67,9 +63,9 @@ def divide_data_to_files():
         block_lines = []
         for line in myfile.readlines():
             index = line.find(';')
-            if(index > 0): # new block
-                if(block_counter > 0): # to avoid empty first block
-                    with open(f"output/bialaczka_{block_counter}.txt", 'w') as resultfile:
+            if(index > 0):  # new block
+                if(block_counter > 0):  # to avoid empty first block
+                    with open(f"{file_prefix}{block_counter}.txt", 'w') as resultfile:
                         resultfile.writelines(block_lines)
                     block_lines = []
                 block_counter += 1
@@ -78,8 +74,10 @@ def divide_data_to_files():
             line = line[index + 1:]
             line = line.replace(';', ' ')
             block_lines.append(line)
-        with open(f"output/bialaczka_{block_counter}.txt", 'w') as resultfile: # to get last block
+        with open(f"{file_prefix}{block_counter}.txt", 'w') as resultfile:  # to get last block
             resultfile.writelines(block_lines)
+    global classes_size
+    classes_size = block_counter
 
 
 def start():
@@ -90,5 +88,5 @@ def start():
         print(f"{item[0]} - {item[1]}")
 
 
-if(__name__=='__main__'):
+if(__name__ == '__main__'):
     start()
